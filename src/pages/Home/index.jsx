23296da1ffdd241cf31de89cbdca22ac32e7c3a5
Matheus <matheus_501@hotmail.com>
2023-05-30 20:08:29 -0300

    fazendo funcionalidade de buscar produtos na lupa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,53 +7,39 @@ import { Footer } from '../../Components/Footer';
 import { Section } from '../../Components/Section';
 import { handleFilter } from '../../utils/handleFilter';
 // import { useParams } from 'react-router-dom';
+
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
 
 export function Home() {
-  // const params = useParams();
-  const { user } = useAuth();
   const [filterText, setFilterText] = useState('');
   const [products, setProducts] = useState([]);
-
-  // const [load, setLoad] = useState(true);
-  // const [error, setError] = useState('');
-
-  // async function getApiData() {
-  //   try {
-  //     const response = await api.get('/products');
-
-  //     setProducts(response.data);
-  //   } catch (error) {
-  //     setError('Api fora do ar');
-  //   } finally {
-  //     setLoad(false);
-  //   }
-  // }
-  // useEffect(() => {
-  //   getApiData();
-  // }, []);
-
-  // if (load) {
-  //   return <h1>...carregando</h1>;
-  // }
-  // if (error) {
-  //   return <h1>{error}</h1>;
-  // }
-
+  const [load, setLoad] = useState(true);
+  const [error, setError] = useState('');
+  const meals = handleFilter('meals', 'name', filterText, products);
+  const desserts = handleFilter('dessert', 'name', filterText, products);
+  const drinks = handleFilter('drinks', 'name', filterText, products);
   async function getProducts() {
     try {
       const response = await api.get('/products');
+
       setProducts(response.data);
     } catch (error) {
-      console.log(error);
-      alert('deu erro');
+      setError('Api fora do ar');
+    } finally {
+      setLoad(false);
     }
   }
-
   useEffect(() => {
     getProducts();
   }, []);
+
+  if (load) {
+    return <h1>...carregando</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
 
   return (
     <>
@@ -69,50 +55,65 @@ export function Home() {
           </div>
         </Box>
 
-        <Section title={'Refeições'}>
-          {products
-            .filter((product) => product.category === 'refeição')
-            .map((product) => (
-              <CardItem
-                key={product.id}
-                filterText={filterText}
-                description={product.description}
-                name={product.name}
-                img={product.img}
-                price={product.price}
-              />
-            ))}
-        </Section>
+        {products.length !== 0 && (
+          <Section title={'Refeições'}>
+            {products
+              .filter((product) => product.category === 'refeição')
+              .filter((product) =>
+                product.name.toLowerCase().includes(filterText.toLowerCase())
+              )
+              .map((product) => (
+                <CardItem
+                  key={product.id}
+                  filterText={filterText}
+                  description={product.description}
+                  name={product.name}
+                  img={product.img}
+                  price={product.price}
+                />
+              ))}
+          </Section>
+        )}
 
-        <Section title={'Sobremesas'}>
-          {products
-            .filter((product) => product.category === 'sobremesa')
-            .map((product) => (
-              <CardItem
-                key={product.id}
-                filterText={filterText}
-                description={product.description}
-                name={product.name}
-                img={product.img}
-                price={product.price}
-              />
-            ))}
-        </Section>
+        {products.length !== 0 && (
+          <Section title={'Sobremesas'}>
+            {products
+              .filter((product) => product.category === 'sobremesa')
+              .filter((product) =>
+                product.name.toLowerCase().includes(filterText.toLowerCase())
+              )
+              .map((product) => (
+                <CardItem
+                  key={product.id}
+                  filterText={filterText}
+                  description={product.description}
+                  name={product.name}
+                  img={product.img}
+                  price={product.price}
+                />
+              ))}
+          </Section>
+        )}
 
-        <Section title={'Bebidas'}>
-          {products
-            .filter((product) => product.category === 'bebida')
-            .map((product) => (
-              <CardItem
-                key={product.id}
-                filterText={filterText}
-                description={product.description}
-                name={product.name}
-                img={product.img}
-                price={product.price}
-              />
-            ))}
-        </Section>
+        {products.length !== 0 && (
+          <Section title={'Bebidas'}>
+            {products
+              .filter((product) => product.category === 'bebida')
+              .filter((product) =>
+                product.name.toLowerCase().includes(filterText.toLowerCase())
+              )
+              .map((product) => (
+                <CardItem
+                  key={product.id}
+                  filterText={filterText}
+                  description={product.description}
+                  name={product.name}
+                  img={product.img}
+                  price={product.price}
+                />
+              ))}
+          </Section>
+        )}
       </Container>
       <Footer />
     </>
